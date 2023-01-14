c8de2fa1b854da48ed538fab148b20495f018f1a
Bby-vivi <114452770+Bby-vivi@users.noreply.github.com>
2023-01-13 22:50:51 -0500

    updates

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,26 @@
+// Working sats
+console.log("working");
+
+// We create the map object with options
+let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+
+// Coordinates for each point to be used in the ployline.
+let line = [
+    [33.9416, -118.4085],
+    [37.6214, -122.3790]
+];
+
+// Create a ployline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "red"
+}).addTo(map);
+
+// We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
+// Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
