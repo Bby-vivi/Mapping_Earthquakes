e778315c37d70a39bcd05605a6000b6a6a0b5513
Bby-vivi <114452770+Bby-vivi@users.noreply.github.com>
2023-01-14 14:22:00 -0500

    updates

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -1,16 +1,15 @@
-// Create the map object with a center and zoom level.
-let map = L.map('mapid', {
-    center: [40.0, -80.0],
-    zoom: 4
-  });
-
-
 // We create the tile layer that will be the background of our map.
 let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid', {
+    center: [40.0, -80.0],
+    zoom: 4
+  });
 
 // Then we add our 'graymap' tile layer to the map.
 light.addTo(map);
